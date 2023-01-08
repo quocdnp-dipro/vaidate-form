289ec3a7fed1cac9f2af638d255947fa85be742e
quocdnp-dipro <quocdnp@dipro.vn>
2023-01-08 23:07:04 +0700

    com

--- src/common/customYup.ts
+++ src/common/customYup.ts
@@ -1,12 +1,17 @@
 import * as yup from "yup";
 import { AnyObject, Maybe } from "yup/lib/types";
 
+//yup.addMethod(yup.string, "nonSpace", function nonSpace() {
+//    return this.transform((value: string, originalValue: string) =>
+//        /^\s/.test(originalValue) ? value.trim() : value
+//    ).required();
+//});
+
 yup.addMethod(yup.string, "nonSpace", function nonSpace() {
     return this.transform((value: string, originalValue: string) =>
         /^\s/.test(originalValue) ? value.trim() : value
     ).required();
 });
-
 declare module "yup" {
     interface StringSchema<
         TType extends Maybe<string> = string | undefined,
